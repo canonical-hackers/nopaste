25cd2de2dfeda1220f229ca1b30ac3104690c740
Brian Haberer <bhaberer@gmail.com>
2012-10-22 13:44:58 -0700

    Changing default template engine to haml. (sorry paulv)

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,11 @@ end
 
 module Nopaste
   class Application < Rails::Application
-    Haml::Filters::CodeRay.encoder_options = {:line_numbers => :inline}
+    config.generators do |g|
+      g.template_engine :haml
+    end
+
+    Haml::Filters::CodeRay.encoder_options = {:line_numbers => :table}
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
