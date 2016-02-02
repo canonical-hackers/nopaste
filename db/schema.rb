f662e781c66d502605cfff0c36b8d325bf7ab7cc
Brian Haberer <bhaberer@gmail.com>
2016-02-01 17:34:31 -0800

    Test skel

--- db/schema.rb
+++ db/schema.rb
@@ -9,41 +9,41 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121030215712) do
+ActiveRecord::Schema.define(version: 20121030215712) do
 
-  create_table "pastes", :id => false, :force => true do |t|
-    t.string   "uuid",        :limit => 36
-    t.text     "content"
-    t.datetime "created_at",                                    :null => false
-    t.datetime "updated_at",                                    :null => false
-    t.string   "language",                  :default => "text"
-    t.string   "description"
-    t.string   "author"
-    t.string   "original_id"
-    t.boolean  "private",                   :default => false
-    t.integer  "user_id"
+  create_table "pastes", id: false, force: :cascade do |t|
+    t.string   "uuid",        limit: 36
+    t.text     "content",     limit: 65535
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "language",    limit: 255,   default: "text"
+    t.string   "description", limit: 255
+    t.string   "author",      limit: 255
+    t.string   "original_id", limit: 255
+    t.boolean  "private",                   default: false
+    t.integer  "user_id",     limit: 4
   end
 
-  create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "username",               :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
-    t.string   "reset_password_token"
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "username",               limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",          limit: 4,   default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
-  add_index "users", ["username"], :name => "index_users_on_username", :unique => true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
 end
